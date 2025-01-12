fecc28b385e0f3cc4a84679c60860ad4296922dc
adz <x12@adz.garden>
2025-01-12 17:04:48 +0100

    Parse command line arguments and add log support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use meshpit::setup_tracing;
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Parser)]
+#[command(
+    name = "meshpit",
+    long_about = None,
+    version
+)]
+struct Args {
+    /// Set log verbosity. Use this for learning more about how your node behaves or for debugging.
+    ///
+    /// Possible log levels are: ERROR, WARN, INFO, DEBUG, TRACE. They are scoped to "meshpit" by
+    /// default.
+    ///
+    /// If you want to adjust the scope for deeper inspection use a filter value, for example
+    /// "=TRACE" for logging _everything_ or "meshpit=INFO,p2panda_net=DEBUG" etc.
+    #[arg(short = 'l', long, value_name = "LEVEL")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    log_level: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    setup_tracing(&args.log_level.unwrap_or_default());
+    Ok(())
 }
---
+++ src/tracing.rs
@@ -0,0 +1,42 @@
+use std::str::FromStr;
+
+use tracing::Level;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+/// Setup logging with the help of the `tracing` crate.
+///
+/// The verbosity and targets can be configured with a filter string:
+///
+/// 1. When no filter is set the default "meshpit=INFO" filter will be applied
+/// 2. When only a level was given ("INFO", "DEBUG", etc.) it will be used for the meshpit target
+///    "meshpit={level}"
+/// 3. When the string begins with an "=", then the log level will be applied for _all_ targets.
+///    This is equivalent to only setting the level "{level}"
+/// 4. When the string specifies a target and a level it will be used as-is: "{filter}", for
+///    example "tokio=TRACE"
+pub fn setup_tracing(filter: &str) {
+    let default = "meshpit=INFO"
+        .parse()
+        .expect("hard-coded default directive should be valid");
+
+    let builder = EnvFilter::builder().with_default_directive(default);
+
+    let filter = if let Some(all_targets_level) = filter.strip_prefix("=") {
+        all_targets_level.to_string()
+    } else {
+        match Level::from_str(&filter) {
+            Ok(level) => format!("meshpit={level}"),
+            Err(_) => filter.to_string(),
+        }
+    };
+    let filter = builder.parse_lossy(filter);
+
+    tracing_subscriber::registry()
+        .with(Layer::default())
+        .with(filter)
+        .try_init()
+        .ok();
+}
